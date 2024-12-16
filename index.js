5606bbffc819b48c0bf7ab9a5821b31ddc000983
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-12-16 18:49:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 /////////////////---------------------------------------workiong live-----------------------------------------------------------------------//////////////////////////////////////// 
  
 
-/////--------------fetch newly created  leads and stpre it on db-------------------------////
+/////--------------fetch newly created  leads and store it on db-------------------------////
 
 
 app.post('/webhook/zoho/leads', async (req, res) => {
